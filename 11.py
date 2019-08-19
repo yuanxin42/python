63f17539e9028989da6b0af84c32ecd40af5d116
yuanxin42 <yuanxin42@xdf.cn>
2019-08-19 17:05:33 +0800

    #

--- 11.py
+++ 11.py
@@ -12,26 +12,29 @@ def custom_threshold(image):
 # 二值化处理
 
 def two_value(imgStr):
-    # 打开文件夹中的图片
-    image=Image.open(imgStr)
-    img = cv2.imread(imgStr)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print(gray)
-    # 灰度图
-    lim=image.convert('L')
-    # 灰度阈值设为165，低于这个值的点全部填白色
-    src1 = cv2.imread(imgStr)
-    threshold = custom_threshold(src1)
-    # threshold=165
-    table=[]
-    
-    for j in range(256):
-        if j<threshold:
-            table.append(0)
-        else:
-            table.append(1)
-
-    bim=lim.point(table,'1')
-    bim.save('./Img2/zzzz.jpg')
+        # 打开文件夹中的图片
+        image=Image.open(imgStr)
+        img = cv2.imread(imgStr)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        print(gray)
+        # 灰度图
+        lim=image.convert('L')
+        # 灰度阈值设为165，低于这个值的点全部填白色
+        src1 = cv2.imread(imgStr)
+        threshold = custom_threshold(src1)
+        # threshold=165
+        table=[]
+        
+        for j in range(256):
+            if j<threshold:
+                table.append(0)
+            else:
+                table.append(1)
+ 
+        bim=lim.point(table,'1')
+        print(bim)
+        bim.save('./Img2/zzzz.jpg')
+        return bim
+        # bim 是一个Image，PIL
  
 two_value('./Img/WechatIMG15.jpeg')
